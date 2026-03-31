cbe85350ec44027276357a9682005e7787583e23
Raghu6798 <courage9605@gmail.com>
2026-03-31 08:31:38 +0530

    added an api_key argument to the QdrantClient instance

--- fastapi_backend/databases/qdrant/qdrant_store.py
+++ fastapi_backend/databases/qdrant/qdrant_store.py
@@ -3,5 +3,5 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-client = QdrantClient(url=os.getenv("QDRANT_URL"))
+client = QdrantClient(url=os.getenv("QDRANT_URL"), api_key=os.getenv("QDRANT_API_KEY"))
 print(client)
