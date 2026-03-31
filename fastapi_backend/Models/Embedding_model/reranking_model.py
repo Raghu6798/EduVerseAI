c638e18e91a1cdbe0065106f0091a39d7992f6d4
Raghu6798 <courage9605@gmail.com>
2026-03-31 08:35:45 +0530

    removed redundant dependencies

--- fastapi_backend/Models/Embedding_model/reranking_model.py
+++ fastapi_backend/Models/Embedding_model/reranking_model.py
@@ -1,11 +1,10 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
 
-
-
 model_name = "colbert-ir/colbertv2.0"
 model_kwargs = {'device': 'cpu'}
 encode_kwargs = {'normalize_embeddings': False}
+
 colBERT = HuggingFaceEmbeddings(
     model_name=model_name,
     model_kwargs=model_kwargs,
